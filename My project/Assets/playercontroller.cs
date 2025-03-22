7d640b4fea95fa3e4dbe9c5a5a70a2c0adbd364c
WarriorDafo <warriordafo13@gmail.com>
2025-03-21 21:46:53 -0600

    enemi

--- My project/Assets/playercontroller.cs
+++ My project/Assets/playercontroller.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    public float velocidad = 5f;
 
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,23 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
 
+        float velocidadY = Input.GetAxis("Vertical") * Time.deltaTime * velocidad;
+
+        animator.SetFloat("movement", velocidadX * velocidad);
+
+        if (velocidadX < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if (velocidadX > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        Vector2 posicion = transform.position;
+
+        transform.position = new Vector2(velocidadX + posicion.x, posicion.y + velocidadY);
     }
 }
